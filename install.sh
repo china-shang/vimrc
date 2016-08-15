31a0eaa5dfdf09ecfbe1762cac2f3dfa3be650b3
china-shang <850747813@qq.com>
2016-08-15 12:09:50 +0800

    correct

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 apt-get update
 apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev mercurial -y
-apt-get remove vim  
-apt-get remove vim-runtime  
-apt-get remove gvim  
-apt-get remove vim-tiny  
-apt-get remove vim-common  
-apt-get remove vim-gui-common  
+apt-get remove vim  -y
+apt-get remove vim-runtime  -y
+apt-get remove gvim  -y
+apt-get remove vim-tiny  -y
+apt-get remove vim-common  -y
+apt-get remove vim-gui-common  -y
 apt-get install ctags -y
 git clone https://github.com/vim/vim.git
 cd vim
@@ -15,7 +15,9 @@ make VIMRUNTIMEDIR=/usr/share/vim/vim74
 make install
 echo "vim has been installed"
 cd ..
-cp .* ~/
+rm ~/.vimrc
+cp .vimrc ~/
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim -c BundleInstall
 apt-get install clang -y
 apt-get install build-essential cmake python-dev -y 
